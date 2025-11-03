7fa150190f0c442124fcd9bccf56155289a585c9
Alejandro1319 <josealejandro1319@gmail.com>
2025-11-02 20:46:37 -0600

    feat(admin): implementa pantalla y formulario de sucursales en el panel administrativo

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import { Menu } from "../pages/Menu/Menu";
+import { Menu } from "../pages/Menu/Menu.jsx";
+import { Sucursales } from "../pages/Sucursales/Sucursales.jsx";
 
 export const AppRouter = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<h1>Raiz</h1>} ></Route>
 			<Route path="/login" element={<h1>Inicio de sesion</h1>} ></Route>
-			<Route path="/menu" element={<Menu />} />
             // TODO: Agregar las demas rutas
+			<Route path="/menu" element={<Menu />} />
+			<Route path="/sucursales" element={<Sucursales />} />
 
             //* Ruta para manejar errores 404
             <Route path="/*" element={<h1>404 - Not Found</h1>} ></Route>
